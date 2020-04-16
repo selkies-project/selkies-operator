d1a24b2fe31fbbd4dabca53bdb966378bdec9b5d
Dan Isla <disla@google.com>
2020-04-16 20:40:10 +0000

    add wait_for_iap script to main cloudbuild

--- setup/infra/sa.tf
+++ setup/infra/sa.tf
@@ -46,6 +46,12 @@ resource "google_project_iam_member" "cluster_service_account-stackdriver-metada
   member  = "serviceAccount:${google_service_account.cluster_service_account.email}"
 }
 
+resource "google_project_iam_member" "cluster_service_account-iap-user" {
+  project = google_project_iam_member.cluster_service_account-metric_writer.project
+  role    = "roles/iap.httpsResourceAccessor"
+  member  = "serviceAccount:${google_service_account.cluster_service_account.email}"
+}
+
 # Grant access to GCR in the same project that the cluster exists in.
 # Make sure workload identity is being used or else this will grant
 # tenants on the cluster access to your GCS buckets, and possibly the terraform state.
---
+++ setup/scripts/wait_for_iap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Copyright 2019 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+SA_EMAIL=$1
+CLIENT_ID=$2
+ENDPOINT=$3
+
+[[ -z "${SA_EMAIL}" || -z "${CLIENT_ID}" || -z "${ENDPOINT}" ]] && echo "USAGE: $0 <sa email> <client id> <endpoint>" && exit 1
+
+TOKEN=$(curl -f -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token" | jq -r '.access_token')
+
+ID_TOKEN=$(curl -f -s -H "Authorization: Bearer ${TOKEN?}" --header 'Content-Type: application/json' -d '{  "audience": "'${CLIENT_ID?}'", "includeEmail": "true" }' "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${SA_EMAIL?}:generateIdToken" | jq -r '.token')
+
+echo "INFO: Waiting for: ${ENDPOINT}"
+count=0
+while [[ "${count}" -le 5 ]]; do
+    STATUS=$(curl --connect-timeout 1 -s -o /dev/null -w "%{http_code}" -H "Authorization: Bearer ${ID_TOKEN?}" "${ENDPOINT?}")
+    [[ "$STATUS" -eq 200 ]] && ((count=count+1))
+    sleep 2
+done
+echo "INFO: Done"
