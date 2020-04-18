20f4beff7f604f371eb0e1338f7b1f642b2fb730
Dan Isla <disla@google.com>
2020-04-18 16:27:24 +0000

    update license headers

--- images/web/dev_server.sh
+++ images/web/dev_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2017 Google Inc. All rights reserved.
+# Google LLC 2019
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- setup/scripts/create_autoscaling_cronjob.sh
+++ setup/scripts/create_autoscaling_cronjob.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2019 Google Inc.
+# Google LLC 2019
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
