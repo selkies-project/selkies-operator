00b5cdcfad1c2a2c93c1eaa124bc24909a503177
Dan Isla <disla@google.com>
2020-04-22 17:28:17 +0000

    update to cloudbuild and wait for IAP script

--- setup/scripts/wait_for_iap.sh
+++ setup/scripts/wait_for_iap.sh
@@ -24,12 +24,15 @@ TOKEN=$(curl -f -s -H "Metadata-Flavor: Google" "http://metadata.google.internal
 
 ID_TOKEN=$(curl -f -s -H "Authorization: Bearer ${TOKEN?}" --header 'Content-Type: application/json' -d '{  "audience": "'${CLIENT_ID?}'", "includeEmail": "true" }' "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${SA_EMAIL?}:generateIdToken" | jq -r '.token')
 
+[[ -z "${ID_TOKEN}" ]] && echo "ERROR: Failed to obtain ID token" && exit 1
+
 echo "INFO: Waiting for: ${ENDPOINT}"
 count=0
 while [[ "${count}" -le 5 ]]; do
     STATUS=$(curl --connect-timeout 1 -s -o /dev/null -w "%{http_code}" -H "Authorization: Bearer ${ID_TOKEN?}" "${ENDPOINT?}")
     [[ "$STATUS" -eq 200 ]] && ((count=count+1))
     [[ "$STATUS" -eq 403 ]] && echo "ERROR: failed to get valid IAP credentials." && exit 1
+    [[ "$STATUS" -eq 302 ]] && echo "ERROR: unexpected 302 from IAP endpoint, possible invalid ID token." && exit 1
     sleep 2
 done
 echo "INFO: Done"
