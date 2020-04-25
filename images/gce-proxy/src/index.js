6007d4b4d24728e434a84e210bf31913775ca95d
Dan Isla <disla@google.com>
2020-04-25 20:15:38 +0000

    import of gce-proxy image

---
+++ images/gce-proxy/src/index.js
@@ -0,0 +1,104 @@
+/*
+ Copyright 2019 Google Inc. All rights reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+const http = require('http')
+httpProxy = require('http-proxy')
+url = require('url');
+
+const getEnvOrDie = (name) => {
+    if (name in process.env) {
+        return process.env[name];
+    }
+    console.log(`Missing env ${name}`);
+    process.exit();
+}
+
+// Get required env vars.
+var CLIENT_ID = getEnvOrDie("CLIENT_ID");
+var BROKER_ENDPOINT = getEnvOrDie("BROKER_ENDPOINT");
+var BROKER_COOKIE = getEnvOrDie("BROKER_COOKIE");
+
+// Create the proxy server.
+var proxy = httpProxy.createProxyServer({
+    target: {
+        protocol: 'https:',
+        host: url.parse(BROKER_ENDPOINT).host,
+        port: 443
+    },
+    changeOrigin: true,
+    ws: true,
+});
+
+// Global token variable that is updated in the background.
+var token = "";
+
+// Function to fetch identity token from GCE metadata server.
+var refreshToken = () => {
+    var refresh = false;
+    if (token === "") {
+        refresh = true;
+    }
+    let toks = token.split(".");
+    if (toks.length === 3) {
+        let payload = JSON.parse(Buffer.from(toks[1], "base64").toString());
+        if (payload.exp - payload.iat < 600) {
+            // Refresh if token is less than 10 minutes from expiring.
+            refresh = true;
+        }
+    } else {
+        // refresh if token is bad.
+        refresh = true;
+    }
+    if (refresh === true) {
+        console.log("INFO: Refreshing token");
+
+        const options = {
+            hostname: 'metadata.google.internal',
+            port: 80,
+            path: '/computeMetadata/v1/instance/service-accounts/default/identity?audience=' + CLIENT_ID + '&format=full',
+            method: 'GET',
+            headers: {
+                'Metadata-Flavor': 'Google'
+            }
+        }
+
+        const req = http.get(options, (res) => {
+            if (res.statusCode !== 200) {
+                console.log("ERROR: Failed to fetch token, status: " + res.statusCode);
+            } else {
+                res.on('data', (d) => {
+                    token = d.toString();
+                });
+            }
+        });
+    }
+}
+
+// Proxy request handler to inject identity token and broker cookie.
+proxy.on('proxyReq', function (proxyReq, req, res, options) {
+    proxyReq.setHeader('Authorization', 'Bearer ' + token);
+    proxyReq.setHeader('Cookie', BROKER_COOKIE);
+});
+
+setInterval(() => {
+    refreshToken();
+}, 10000);
+
+// Get initial token
+refreshToken();
+
+console.log("INFO: listening on port 5050")
+proxy.listen(5050);
