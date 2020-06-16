baed6e263f4ee6097bfeac7909e66185dcf05eba
Dan Isla <disla@google.com>
2020-06-16 16:49:07 +0000

    add retries to gcloud alpha commands when creating oauth client

--- setup/scripts/create_oauth_client.sh
+++ setup/scripts/create_oauth_client.sh
@@ -33,12 +33,24 @@ APP_NAME=$1
 # Ensure that the API is enabled.
 $GCLOUD services enable iap.googleapis.com >/dev/null
 
+# Helper function that retries failed alpha commands up to 5 times.
+function gcloud_alpha() {
+    local count=0
+    while [[ $count -lt 5 ]]; do
+        gcloud -q alpha $@
+        [[ $? -eq 0 ]] && return 0
+        ((count=count+1))
+        sleep 2
+    done
+    return 1
+}
+
 # Check to see if OAuth brand already exists
 log_cyan "INFO: Creating OAuth Brand"
-BRAND_ID=$($GCLOUD alpha iap oauth-brands list --filter="applicationTitle~'${APP_NAME?}'" --format='value(name)')
+BRAND_ID=$(gcloud_alpha iap oauth-brands list --format='value(name)' --limit=1)
 if [[ -z "${BRAND_ID}" ]]; then
     # Create the OAuth Brand
-    BRAND_ID=$($GCLOUD alpha iap oauth-brands create --application_title="${APP_NAME?}" --support_email=$($GCLOUD config get-value account) --format='value(name)')
+    BRAND_ID=$(gcloud_alpha iap oauth-brands create --application_title="${APP_NAME?}" --support_email=$($GCLOUD config get-value account) --format='value(name)')
     [[ $? -ne 0 ]] && echo "ERROR: Failed to create oauth brand." && exit 1
     sleep 2
 else
@@ -48,10 +60,10 @@ fi
 # Check to see if OAuth client already exists.
 CLIENT_ID=
 CLIENT_SECRET=
-IFS=',' read -ra toks < <($GCLOUD alpha iap oauth-clients list ${BRAND_ID?} --filter="displayName~'${APP_NAME?}'"  --limit=1 --format 'csv[no-heading](name,secret)') 
+IFS=',' read -ra toks < <(gcloud_alpha iap oauth-clients list ${BRAND_ID?} --filter="displayName~'${APP_NAME?}'"  --limit=1 --format 'csv[no-heading](name,secret)') 
 if [[ ${#toks[@]} -eq 0 ]]; then
     log_cyan "INFO: Creating OAuth client"
-    IFS=',' read -ra toks < <($GCLOUD alpha iap oauth-clients create ${BRAND_ID?} --display_name="${APP_NAME?}" --format 'csv[no-heading](name,secret)')
+    IFS=',' read -ra toks < <(gcloud_alpha iap oauth-clients create ${BRAND_ID?} --display_name="${APP_NAME?}" --format 'csv[no-heading](name,secret)')
     [[ $? -ne 0 ]] && echo "ERROR: Failed to create oauth client." && exit 1
     CLIENT_ID=$(basename ${toks[0]})
     CLIENT_SECRET=${toks[1]}
