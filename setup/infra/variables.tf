99186288e17bba0dccd9ca5ab744fbeef5221577
Reisbel Machado <rmachado@itopia.com>
2021-05-04 10:40:30 -0400

    Support for Custom GCLB SSL Policy

--- setup/infra/variables.tf
+++ setup/infra/variables.tf
@@ -31,3 +31,26 @@ variable "additional_ssl_certificate_domains" {
   type        = list
   default     = []
 }
+
+variable "custom_ssl_policy_enabled" {
+  description = "Enable custom SSL Policy"
+  default     = true
+}
+
+variable "ssl_policy_min_tls_version" {
+  description = "(optional) - The minimum version of SSL protocol that can be used by the clients\nto establish a connection with the load balancer. Default value: \"TLS_1_0\" Possible values: [\"TLS_1_0\", \"TLS_1_1\", \"TLS_1_2\"]"
+  type        = string
+  default     = "TLS_1_0"
+}
+
+variable "ssl_policy_profile" {
+  description = "(optional) - Profile specifies the set of SSL features that can be used by the\nload balancer when negotiating SSL with clients. If using 'CUSTOM',\nthe set of SSL features to enable must be specified in the\n'customFeatures' field.\n\nSee the [official documentation](https://cloud.google.com/compute/docs/load-balancing/ssl-policies#profilefeaturesupport)\nfor information on what cipher suites each profile provides. If\n'CUSTOM' is used, the 'custom_features' attribute **must be set**. Default value: \"COMPATIBLE\" Possible values: [\"COMPATIBLE\", \"MODERN\", \"RESTRICTED\", \"CUSTOM\"]"
+  type        = string
+  default     = "COMPATIBLE"
+}
+
+variable "ssl_policy_custom_features" {
+  description = "(optional) - Profile specifies the set of SSL features that can be used by the\nload balancer when negotiating SSL with clients. This can be one of\n'COMPATIBLE', 'MODERN', 'RESTRICTED', or 'CUSTOM'. If using 'CUSTOM',\nthe set of SSL features to enable must be specified in the\n'customFeatures' field.\n\nSee the [official documentation](https://cloud.google.com/compute/docs/load-balancing/ssl-policies#profilefeaturesupport)\nfor which ciphers are available to use. **Note**: this argument\n*must* be present when using the 'CUSTOM' profile. This argument\n*must not* be present when using any other profile."
+  type        = set(string)
+  default     = null
+}
