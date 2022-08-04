66f693bc4bcc53981674ca537f9d7abc08e22ec5
deepak <ddalavi@itopia.com>
2022-08-04 18:51:17 +0530

    selkies copyright

--- setup/infra/image-streaming/sa.tf
+++ setup/infra/image-streaming/sa.tf
@@ -1,9 +1,16 @@
-resource "google_project_iam_member" "cluster_service_account-artifact-registry" {
-  project = var.project_id
-  role     = "roles/artifactregistry.reader"
-  member   = "serviceAccount:${data.google_service_account.broker_cluster.email}"
-}
-
+# Copyright 2022 The Selkies Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 resource "google_project_service" "containerfilesystem" {
   for_each = toset(var.gcp_service_list)
   
@@ -16,4 +23,5 @@ resource "google_project_service" "containerfilesystem" {
   }
 
   disable_dependent_services = true
+  disable_on_destroy = false
 }
